47d1adb3405fc5aaa07e8c71d45c85813ff03d3b
Tornado <519206112@qq.com>
2019-06-30 17:25:05 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -1,5 +1,20 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+  echo "Hello World."
+
+  pipeline {
+    agent any
+
+    stages {
+        stage('test') {
+		     ansiblePlaybook(
+		        inventory: 'local_inventory/hosts.cfg',
+		        playbook: 'cloud_playbooks/create-aws.yml',
+		        extraVars: [
+		            login: 'mylogin',
+		            secret_key: [value: 'g4dfKWENpeF6pY05', hidden: true]
+		        ])}
+    }
+  }
 }
