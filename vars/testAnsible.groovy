f9601a75d223dd017eabf941f4dcc7abb0ac3fd0
Tornado <519206112@qq.com>
2019-06-30 21:53:04 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -11,7 +11,8 @@ def call() {
 		steps {
 		     ansiblePlaybook(
 		        inventory: '/var/pipeline-library/ansible/hosts',
-		        playbook: '/var/pipeline-library/ansible/tasks/main.yml'
+		        playbook: '/var/pipeline-library/ansible/tasks/main.yml',
+			extras: '-e project_name="some value"'
 	}
     }
   }
