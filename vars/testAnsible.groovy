aca4dc991ef3eeed99ffbf7456e0cde54df8af22
Tornado <519206112@qq.com>
2019-07-02 10:40:06 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -17,8 +17,8 @@ def call() {
                       sh "export ANSIBLE_CONFIG=${params.ANSIBLE_HOME}"
                   
                       ansiblePlaybook( 
-                          playbook: '${params.ANSIBLE_HOME}/tasks/main.yml',
-                          inventory: '${params.ANSIBLE_HOME}/hosts', 
+                          playbook: "${params.ANSIBLE_HOME}/tasks/main.yml",
+                          inventory: "${params.ANSIBLE_HOME}/hosts", 
                           extras: '-e parameter="some value"')
                   
                       sh 'echo $ANSIBLE_CONFIG'
