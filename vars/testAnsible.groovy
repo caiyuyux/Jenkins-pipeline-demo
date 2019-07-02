526fabd728e0d8a4c44c7aa80ebb630df99e53a3
Tornado <519206112@qq.com>
2019-07-02 18:57:10 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -13,6 +13,10 @@ def call() {
             stage('test ansible') {
 
                 steps {
+                  ansiblePlaybook( 
+                          playbook: "${params.ANSIBLE_HOME}/tasks/main.yml",
+                          inventory: "${params.ANSIBLE_HOME}/hosts", 
+                          extras: '-e parameter="some value"')
                       // 设置环境变量
                        sh 'ansible-playbook /var/pipeline-library/ansible/tasks/main.yml -i /var/pipeline-library/ansible/hosts -e "parameter=some value"'         
                 }
