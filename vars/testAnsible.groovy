170ca2fb75b0331acc2ff481239b7648ee33ce65
Tornado <519206112@qq.com>
2019-07-02 09:33:52 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -14,9 +14,9 @@ def call() {
 
                 steps {
                       // 切换到 ansible 主目录
-                      sh 'cd ${params.ANSIBLE_HOME}'
+                      sh "cd ${params.ANSIBLE_HOME}"
                   
-                      sh 'ansible-playbook ${params.ANSIBLE_HOME}/tasks/main.yml -i ${params.ANSIBLE_HOME}/hosts -e "project_name=some value"'
+                      sh "ansible-playbook ${params.ANSIBLE_HOME}/tasks/main.yml -i ${params.ANSIBLE_HOME}/hosts -e 'project_name=some value'"
                     }             
                 }
             }
