0cfe26295e9ca844ecbd99bdaf119699e39a3075
Tornado <519206112@qq.com>
2019-07-02 09:59:24 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -13,11 +13,9 @@ def call() {
             stage('test ansible') {
 
                 steps {
-                      sh 'pwd | xargs echo'
-                      // 切换到 ansible 主目录
-                      sh "cd ${params.ANSIBLE_HOME}"
+                      // 设置环境变量
+                      sh "export ANSIBLE_CONFIG=${params.ANSIBLE_HOME}/ansible.cfg"
                   
-                      sh 'pwd | xargs echo'                  
                       sh "ansible-playbook ${params.ANSIBLE_HOME}/tasks/main.yml -i ${params.ANSIBLE_HOME}/hosts -e 'project_name=some'"
                     }             
                 }
