7a4adbb4a0770aab9f990dcc515b507610a90477
Tornado <519206112@qq.com>
2019-07-03 09:32:24 +0800

    Update testAnsible.groovy

--- vars/testAnsible.groovy
+++ vars/testAnsible.groovy
@@ -16,7 +16,7 @@ def call() {
                           playbook: "/var/pipeline-library/ansible/tasks/main.yml",
                           inventory: "/var/pipeline-library/ansible/hosts", 
                           extraVars: [
-                              hosts: "${animal}",
+                              animal: "${animal}",
                               project: "test"
                     ])
                 }
