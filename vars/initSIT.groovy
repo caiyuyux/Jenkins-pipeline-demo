4c0023952879f7dc27bdae0c1216d841a449b18a
caiyuyu <caiyuyux@gmail.com>
2019-07-04 14:13:59 +0800

    t

---
+++ vars/initSIT.groovy
@@ -0,0 +1,80 @@
+#!/usr/bin/env groovy
+
+def projects = ["java_base", "java_api"]
+def parallelStagesMap = projects.collectEntries {
+  ["${it}" : generateStage(it)]
+}
+
+def generateStage(project) {
+  return {
+    stage("stage: ${project}") {
+      sh script: "sleep 15"
+    }
+  }
+}
+
+
+def call() {
+
+  echo "准备开始初始化 SIT. 初始化动物:${animal}."
+
+  pipeline {
+    agent any
+
+    tools {
+      maven 'apache-maven-3.0.1' 
+    }
+
+    stages {
+
+      stage('Checkout') {
+
+        echo '初始化新分支: ${branch}'
+
+        steps {
+          script {
+            parallel parallelStagesMap
+          }
+        }             
+
+      }
+
+      stage('InitTapd') {
+
+        echo '修改 Tapd '
+        // 爬虫
+
+      }
+
+      stage('Ansible') {
+
+      }
+
+      stage('构建项目') {
+
+        env.PATH = "${mvnHome}/bin:${env.PATH}"
+
+
+        // 并行
+
+        steps {
+
+          sh 'pwd'
+          ansiblePlaybook( 
+            playbook: "/var/pipeline-library/ansible/tasks/main.yml",
+            inventory: "/var/pipeline-library/ansible/hosts", 
+            extraVars: [
+              host: "${animal}",
+              project: "${project}"
+            ])
+        }
+      }
+    }
+
+    post { 
+      always { 
+        echo 'I will always say Hello again!'
+      }
+    }
+  }
+}
